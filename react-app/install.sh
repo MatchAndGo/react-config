62087307451471f23ab4e539211e0bb025c78c0b
IagoLast <iago.lastra@gmail.com>
2021-09-05 13:04:28 +0200

    feat: Add silent mode

--- react-app/install.sh
+++ react-app/install.sh
@@ -1,7 +1,7 @@
-curl -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.editorconfig
-curl -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.eslintrc.js
-curl -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.prettierrc.js
-curl -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/stylelint.config.js
-curl -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/tsconfig.json
+curl -s -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.editorconfig
+curl -s -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.eslintrc.js
+curl -s -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/.prettierrc.js
+curl -s -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/stylelint.config.js
+curl -s -O -J https://raw.githubusercontent.com/MatchAndGo/react-config/main/react-app/tsconfig.json
 
-npm i -D eslint prettier stylelint eslint-config-react-app eslint-config-prettier stylelint-config-property-sort-order-smacss stylelint-config-prettier stylelint-config-css-modules
+npm i --quiet -D eslint prettier stylelint eslint-config-react-app eslint-config-prettier stylelint-config-property-sort-order-smacss stylelint-config-prettier stylelint-config-css-modules
